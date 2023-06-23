fb9ed256d24db2f4fd22bec258318b54b83f6c28
DESKTOP-P678S41\wmh <dnjsalsgh123@gmail.com>
2023-06-24 07:54:54 +0900

    project initialize

--- App.tsx
+++ App.tsx
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Open up App.tsx to start working on your app! testtt</Text>
+      <div> 안녕하세요</div>
       <StatusBar style="auto" />
     </View>
   );
